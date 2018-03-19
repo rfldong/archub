ee14047ed49c1ca3624edf152e0c23dcb886c0e3
Don Grote <dgrote@refirmlabs.com>
2018-03-19 10:28:36 -0400

    Removed archub.types

--- archub/commands/todo.py
+++ archub/commands/todo.py
@@ -2,8 +2,13 @@ import os
 import sys
 from argparse import ArgumentParser
 from archub import cmdline, config
-from archub.types import GithubRepository
+from github import Github
 from github.GithubObject import NotSet
+
+def get_repo(github, org, repo_name):
+    if len(org) > 0:
+        return github.get_organization(org).get_repo(repo_name)
+    return github.get_user().get_repo(repo_name)
 
 def main(args):
     parser = ArgumentParser(
@@ -11,15 +16,17 @@ def main(args):
         description='Quickly create an issue ticket for the specified repository'
     )
     parser.add_argument('-o', '--org', required=False, type=str,
-        default=config.GITHUB_ORGANIZATION, help='[default: "{}"]'.format(config.GITHUB_ORGANIZATION))
-    parser.add_argument('-r', '--repo', required=True, type=GithubRepository)
+        default=config.GITHUB_ORGANIZATION,
+        help='empty string for no organization [default: "{}"]'.format(config.GITHUB_ORGANIZATION))
+    parser.add_argument('-r', '--repo', required=True, type=str)
     parser.add_argument('-t', '--title', required=False, type=str, default=NotSet)
     parser.add_argument('-a', '--assign', required=False, type=str, default=NotSet)
     parser.add_argument('description')
     parsed_args = parser.parse_args(args)
     issue_title = parsed_args.description if parsed_args.title is NotSet else parsed_args.title
     issue_description = parsed_args.description if parsed_args.title is not NotSet else NotSet
-    parsed_args.repo.create_issue(
+    repo = get_repo(Github(config.GITHUB_TOKEN), parsed_args.org, parsed_args.repo)
+    repo.create_issue(
         issue_title,
         body=issue_description,
         assignee=parsed_args.assign)
