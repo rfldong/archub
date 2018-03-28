3bc431b4880d8334d25453db4f19279b85b36025
Don Grote <dgrote@refirmlabs.com>
2018-03-28 11:44:15 -0400

    Implemented support for short-commands.
    Fixes #19.
    Removed task/tasks aliases as short-commands naturally provide the same functionality with greater flexibility.

--- archub/commands/__init__.py
+++ archub/commands/__init__.py
@@ -1,11 +1,9 @@
 from .todo import main as todo
-from .task import main as task
 from .task import main as tasks
 from .branch import main as branch
 
 __all__ = [
     'todo',
-    'task',
     'tasks',
     'branch',
 ]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='archub',
-    version='1.0.0a5',
+    version='1.0.0a6',
     install_requires=[
         'requests',
         'pygithub',
