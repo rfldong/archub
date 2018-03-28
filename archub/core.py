6d8e058ec6e2ea6de21b61721ea3bf141cae24dc
Don Grote <dgrote@refirmlabs.com>
2018-03-28 15:39:49 -0400

    If pull request already exists, just update the remote branch. If the pull request already exists and is closed, reopen it. If the repository is dirty, abort.

--- archub/core.py
+++ archub/core.py
@@ -117,8 +117,9 @@ def branch(issuenum_or_branchname):
 
 def pullrequest(title, body='', base='master', maintainer_can_modify=True):
     repo = config.repo
+    assert repo.is_dirty() is False, 'Your branch is dirty; commit your changes and try again.'
     remote = repo.remote()
-    assert remote.exists(), 'Repository remote doesn\'t exist; aborting'
+    assert remote.exists(), 'Repository remote doesn\'t exist; aborting.'
     # push the current branch to origin as a remote branch (if it doesn't already exist)
     ret = remote.push('refs/heads/{0}:refs/heads/{0}'.format(repo.active_branch.name))
     repo.active_branch.set_tracking_branch(
@@ -142,6 +143,14 @@ def pullrequest(title, body='', base='master', maintainer_can_modify=True):
             repo.active_branch.name,
             maintainer_can_modify=maintainer_can_modify)
     else:
+        try:
+            pr = github_repo.get_pull(config.GITHUB_ISSUE_NUMBER)
+            if 'closed' == pr.state:
+                pr.edit(state='open')
+                return
+        except Exception:
+            # pull request already exists, do nothing
+            return
         github_repo.create_pull(
             issue,
             base,
